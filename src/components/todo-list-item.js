b6a9466b992789c1937e92a0ad209975a8348fc2
Anton Romankov <romant094@gmail.com>
2019-02-05 16:23:06 +0300

    Added control buttons to list item

--- src/components/todo-list-item.js
+++ src/components/todo-list-item.js
@@ -5,11 +5,23 @@ import './todo-list-item.css';
 const TodoListItem = ({ label, important = false }) => {
 
     const spanStyle = {
-        color: important ? 'tomato' : 'black'
+        color: important ? 'steelblue' : 'black',
+        fontWeight: important ? 'bold' : 'normal'
     }
 
     return (
-        <span className="todo-list-item" style={spanStyle}>{label}</span>
+        <>
+            <span className="todo-list-item" style={spanStyle}>{label}</span>
+            <div className="todo-list-buttons">
+                {/* TODO add icons into buttons */}
+                <button className="btn btn-outline-danger btn-sm">
+                    delete
+                </button>
+                <button className="btn btn-outline-success btn-sm">
+                    !
+                </button>
+            </div>
+        </>
     );
 }
 
