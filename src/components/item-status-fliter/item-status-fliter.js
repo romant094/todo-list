052e32ad99bd5bcee1bff3af8a6b702ec0614fe7
Anton Romankov <romant094@gmail.com>
2019-02-05 17:16:08 +0300

    Todo list and todo list item are now classes

--- src/components/item-status-fliter/item-status-fliter.js
+++ src/components/item-status-fliter/item-status-fliter.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './item-status-fliter.css';
 
-const Itemstatusfliter = () => {
-    return (
-        <div className="btn-group">
-            <button className="btn btn-info">All</button>
-            <button className="btn btn-outline-secondary">Active</button>
-            <button className="btn btn-outline-secondary">Done</button>
-        </div>
-    )
+export default class Itemstatusfliter extends Component {
+    render() {
+        return (
+            <div className="btn-group">
+                <button className="btn btn-info">All</button>
+                <button className="btn btn-outline-secondary">Active</button>
+                <button className="btn btn-outline-secondary">Done</button>
+            </div>
+        )
+    }
 }
-
-export default Itemstatusfliter;
--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,28 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './todo-list-item.css';
 
-const TodoListItem = ({ label, important = false }) => {
+export default class TodoListItem extends Component {
 
-    const spanStyle = {
-        color: important ? 'steelblue' : 'black',
-        fontWeight: important ? 'bold' : 'normal'
+    render() {
+        const { label, important = false } = this.props;
+
+        const spanStyle = {
+            color: important ? 'steelblue' : 'black',
+            fontWeight: important ? 'bold' : 'normal'
+        }
+
+        return (
+            <>
+                <span className="todo-list-item" style={spanStyle}>{label}</span>
+                <div className="todo-list-buttons">
+                    {/* TODO add icons into buttons */}
+                    <button className="btn btn-outline-danger btn-sm">delete</button>
+                    <button className="btn btn-outline-success btn-sm">!</button>
+                </div>
+            </>
+        )
     }
-
-    return (
-        <>
-            <span className="todo-list-item" style={spanStyle}>{label}</span>
-            <div className="todo-list-buttons">
-                {/* TODO add icons into buttons */}
-                <button className="btn btn-outline-danger btn-sm">
-                    delete
-                </button>
-                <button className="btn btn-outline-success btn-sm">
-                    !
-                </button>
-            </div>
-        </>
-    );
 }
-
-export default TodoListItem;
