5745e1f9cdec6bf9f04f566f2bcfd16090762147
Anton Romankov <romant094@gmail.com>
2019-02-05 16:23:22 +0300

    Added some styles to heaer

--- src/components/app-header.js
+++ src/components/app-header.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './app-header.css';
 
 const AppHeader = () => {
     return (
-        <div>
+        <div className='header'>
             <h1>ToDo list</h1>
             <span> {1} more to do, {3} done</span>
         </div>
