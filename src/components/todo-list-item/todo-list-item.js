ce95d38569423a8f8a8dddde7043e05f3bfff239
Anton Romankov <romant094@gmail.com>
2019-02-06 15:31:54 +0300

    refactoring

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,53 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './todo-list-item.css';
 
-export default class TodoListItem extends Component {
-    // New standard
-    state = {
-        done: false,
-        important: false
-    };
-    onLabelClick = () => {
-        this.setState(({done}) => {
-            return{
-                done: !done
-            }
-        });
-    };
-    onMarkImportant = () => {
-        this.setState(({important}) => {
-            return{
-                important: !important
-            }
-        });
-    };
-
-    // end of new standard
-
-    render() {
-        const { label, onDeleted } = this.props;
-        const { done, important } = this.state;
-        let classNames = 'todo-list-item';
-        if (done) {
-            classNames += ' done';
-        }
-        if (important) {
-            classNames += ' important';
-        }
-
-        return (
-            <>
-                <span className={classNames}>
-                    <span className='todo-list-item-label' onClick={this.onLabelClick}>{label}</span>
-                </span>
-                <div className="todo-list-buttons">
-                    {/* TODO add icons into buttons */}
-                    <button className="btn btn-outline-danger btn-sm"
-                    onClick={onDeleted}>delete</button>
-                    <button className="btn btn-outline-success btn-sm" onClick={this.onMarkImportant}>!</button>
-                </div>
-            </>
-        )
+const TodoListItem = (props) => {
+    const { label, onDeleted, onToggleImportant, onToggleDone, done, important } = props;
+    let classNames = 'todo-list-item';
+    if (done) {
+        classNames += ' done';
     }
+    if (important) {
+        classNames += ' important';
+    }
+
+    return (
+        <>
+            <span className={classNames}>
+                <span className='todo-list-item-label'
+                    onClick={onToggleDone}>
+                    {label}
+                </span>
+            </span>
+            <div className="todo-list-buttons">
+                {/* TODO add icons into buttons */}
+                <button className="btn btn-outline-danger btn-sm"
+                    onClick={onDeleted}>delete</button>
+                <button className="btn btn-outline-success btn-sm"
+                    onClick={onToggleImportant}>!</button>
+            </div>
+        </>
+    )
 }
+
+export default TodoListItem;
--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -3,7 +3,7 @@ import React from 'react';
 import TodoListItem from '../todo-list-item';
 import './todo-list.css';
 
-const TodoList = ({ todos, onDeleted }) => {
+const TodoList = ({ todos, onDeleted, onToggleImportant, onToggleDone }) => {
 
     const elements = todos.map((item) => {
         const { id } = item;
@@ -11,7 +11,10 @@ const TodoList = ({ todos, onDeleted }) => {
             <li className='list-group-item' key={id}>
                 <TodoListItem
                     {...item}
-                    onDeleted={()=>onDeleted(id)}/>
+                    onDeleted={() => onDeleted(id)}
+                    onToggleImportant={() => onToggleImportant(id)}
+                    onToggleDone={() => onToggleDone(id)}
+                />
             </li>
         );
     });
